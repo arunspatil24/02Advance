45de4c83858a71dee2c0be4fb036159417ea1abc
arunspatil24 <33492857+arunspatil24@users.noreply.github.com>
2019-05-06 09:18:25 +0530

    Add project files.

---
+++ 02Advance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02Advance
+{
+    [Author("Guru")]
+    class Program
+    {
+        [Author("Arun")]
+        static void Main(string[] args)
+        {
+            int c = Add(10, 20);
+            Console.WriteLine(c);
+        }
+
+        [Obsolete(message:"Hello")]
+        public static int Add(int a,int b)
+        {
+            return a + b;
+        }
+    }
+    [System.AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+    sealed class AuthorAttribute : Attribute
+    {
+        public AuthorAttribute(string author)
+        {
+            Author = author;
+        }
+        public string Author { get; set; }
+    }
+}
---
+++ CustomeExcpetionDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomeExcpetionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student() { Name = "Arun", Age = -25 };
+            try
+            {
+                ValidateAge(s.Age);
+            }
+            catch(InvaildAgeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine(s.Name+"\t"+s.Age);   
+        }
+        public static void ValidateAge(int age)
+        {
+                if (age < 0)
+                    throw new InvaildAgeException();
+        }
+    }
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+    class InvaildAgeException:Exception
+    {
+        public InvaildAgeException():base("Invalid Age")
+        {
+        }
+
+
+    }
+
+}
